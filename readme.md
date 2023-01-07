d2863d6886d5dc8171d297ad22f74245db38b054
suryasai <suryasaichanti@gmail.com>
2023-01-07 17:08:01 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+## Quiz App
+
+A simple quiz app that allows users to test their knowledge created with html css and javascript.
+
+
+[Live link](https://aquamarine-macaron-ee89ba.netlify.app/) 
+
+![](./Image/Quiz_app.png)
---
+++ script.js
@@ -0,0 +1,118 @@
+const quizData = [
+    {
+        question: "Which built-in method calls a function for each element in the array?",
+        a: "while()",
+        b: "loop()",
+        c: "forEach()",
+        d: "None of the above",
+        correct: "c",
+    },
+    {
+        question: "Which built-in method reverses the order of the elements of an array?",
+        a: "changeOrder(order)",
+        b: "reverse()",
+        c: "sort(order)",
+        d: "None of the above",
+        correct: "b",
+    },
+    {
+        question: "Which of the following is a valid type of function javascript supports?",
+        a: "named function",
+        b: "anonymous function",
+        c: "Both the above",
+        d: "None of the above",
+        correct: "c",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+
+const quiz = document.getElementById("quiz") ;
+const answerEls = document.getElementsByClassName("answer");
+const questionEl = document.getElementById("question");
+const a_text= document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+questionEl.innerHTML =quizData[currentQuiz ].question;
+a_text.textContent = quizData[currentQuiz].a
+b_text.textContent = quizData[currentQuiz ].b
+c_text.textContent = quizData[currentQuiz].c;
+d_text.textContent = quizData[currentQuiz].d
+ deselectAnswers();
+    
+}
+
+function deselectAnswers() {
+   
+    let arr = [...answerEls]
+  
+    arr.forEach((e,i)=>{
+        e.checked=false;
+    })
+}
+
+function getSelected() {
+    let arr = [...answerEls];
+    let selectedId = ''; 
+    arr.forEach((e, i) => {
+      if (e.checked) {
+        selectedId = e.id;
+        return;
+      }
+    });
+    return selectedId;
+  }
+  
+
+  submitBtn.addEventListener('click', () => { 
+    let selected = getSelected();
+    console.log(selected);
+    if(!selected){
+        alert("please select  an option")
+        return
+    }
+    if(selected === quizData[currentQuiz].correct){
+        score++;
+    }
+    currentQuiz++
+    if(currentQuiz< quizData.length){
+        loadQuiz()
+    }
+    else{
+        quiz.innerHTML = ` <h2>You answered correctly at ${score}/${quizData.length} </h2>`
+    }
+    
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
